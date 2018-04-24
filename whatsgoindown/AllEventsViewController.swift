b55165e2ef1665d311b6efa6cfd5886835275c9a
Michell Li <li.michell12@gmail.com>
2018-04-24 10:30:33 -0500

    alleventscontroller

--- whatsgoindown/AllEventsViewController.swift
+++ whatsgoindown/AllEventsViewController.swift
@@ -8,30 +8,50 @@
 
 import UIKit
 
-class AllEventsViewController: UIViewController {
+class AllEventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
+    var eventsList = [Event]()
+    @IBOutlet weak var tableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let count = DataStore.shared.count()
+        var i = 0
+        while i < count {
+            eventsList.append(DataStore.shared.getEvent(index: i))
+            i += 1
+        }
+        print(count)
+        print(eventsList)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(DataStore.shared.count())
+        return DataStore.shared.count()
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "allid", for: indexPath)
+        
+        let event = DataStore.shared.getEvent(index: indexPath.row)
+        cell.textLabel?.text = event.eventName
+        cell.detailTextLabel?.text =  event.eventAddress
+        print("cell.textLabel?.text!")
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? EventDetailViewController,
+            let selectedIndexPath = self.tableview.indexPathForSelectedRow {
+            destinationVC.event = DataStore.shared.getEvent(index: selectedIndexPath.row)
+            // Set the delegate (self = this object).
+            destinationVC.delegate = self
+        }
+    }
 
 }
